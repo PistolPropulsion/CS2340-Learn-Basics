1ef8179560407712f74aeb256c4d61c3b234ed45
vab10266 <vaudalexburton@gmail.com>
2018-01-26 14:06:15 -0500

    fixed name

--- M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -73,7 +73,7 @@ import edu.gatech.oad.antlab.pkg2.*;
                   toPrint += p4.toString("jjoseprabu3");
                   //Person5 replace P4 with your name
                   //and gburdell5 with your gt id
-                  Person5 p5 = new Person5("Vaud");
+                  Person5 p5 = new Person5("Vaud Burton");
                   toPrint += p5.toString("vburton3");
 		  
         System.out.println(toPrint);
